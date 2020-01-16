1214f66f2496a6eb5968764acd0c11238e1f1119
Chayka <58815424+kamome-inc@users.noreply.github.com>
2020-01-16 17:00:47 +0300

    Доработано

--- frequency.py
+++ frequency.py
@@ -13,18 +13,32 @@ for line in lines:
     temp1 = line.split(' ')
     for word in temp1:
         mass_words.append(word.lower())
-num_of_word = len(mass_words)
+
 
 for word in mass_words:
-    if mass_valued_words.count(word) != 1:
-        mass_valued_words.append(word)
-        mass_values.append(1)
-    else:
-        i = mass_valued_words.index(word)
-        mass_values[i] += 1
+    if len(word) > 4:
+        if mass_valued_words.count(word) != 1:
+            mass_valued_words.append(word)
+            mass_values.append(1)
+        else:
+            i = mass_valued_words.index(word)
+            mass_values[i] += 1
+
+num_of_word = len(mass_values)
+all_words = len(mass_words)
+for i in range(num_of_word-1):
+    for j in range(num_of_word-i-1):
+        if mass_values[j] < mass_values[j+1]:
+            mass_values[j], mass_values[j+1] = mass_values[j+1], mass_values[j]
+            mass_valued_words[j], mass_valued_words[j + 1] = mass_valued_words[j + 1], mass_valued_words[j]
+            # print(mass_valued_words[j + 1], mass_valued_words[j])
 
 # Блок вывода всей тестовой хрени
-print(num_of_word)
-print(mass_words)
-print(mass_valued_words)
-print(mass_values)
+print('Уникальных слов в тексте: ', num_of_word)
+sum = 0
+for i in range(num_of_word):
+    coeff = mass_values[i]/all_words*100
+    sum += coeff
+    print(mass_valued_words[i], ' ', mass_values[i], ' ', coeff, '%')
+
+print(sum)
